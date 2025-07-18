98fde303308e9950c3fd34075c6ba35c578734d2
Arthuryh <ayhayakawa@gmail.com>
2025-07-18 16:29:07 -0300

    Implemented destroy mechanic (shot>asteroid)

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ def main():
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
         for d in drawable:
